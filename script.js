9f61c9516f74690df106f903dea6d21b19471e03
hamdan_abdul <hamdan@anvayaanalytics.com>
2020-06-11 10:30:43 +0530

    nav bar and card

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
 // console.log(input.value)
-const lists=document.querySelector("#list")
-function print(){
-    toprint=input.value;
-    const listitems=
-    `<li>
-    <p class="comments">${toprint}
-    </p>
-    </li>`
-    lists.insertAdjacentHTML("beforeend",listitems)
-    input.value="";
+const lists = document.querySelector("#list");
+function printcomment() {
+  toprint = input.value;
+  if (toprint) {
+    const listitems = `<li>
+        <p class="comments">${toprint}
+        </p>
+        
+        </li>`;
+    lists.insertAdjacentHTML("beforeend", listitems);
+    input.value = "";
+  }
 }
+document.addEventListener("keyup", function () {
+  if (event.keyCode == 13) {
+    printcomment();
+  }
+});
